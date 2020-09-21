e27ed3256e5b1f4f5c73954a3bd0a499297a7e0a
Erik Matos <erik_senac@hotmail.com>
2020-09-21 11:44:06 -0300

    Adiciona degit

--- bin/scaffolding.sh
+++ bin/scaffolding.sh
@@ -17,13 +17,12 @@ read TEXT
 PROJECT="$(echo -e "${TEXT}" | tr -d '[:space:]')"
 mkdir -p $PROJECT
 echo -e → Criando pastas...
-git clone https://github.com/ErikGMatos/template-react-erik.git templatetemp &&  cp -r ./templatetemp/. ./$PROJECT  && rm -rf ./templatetemp && cd $PROJECT && yarn
+npx degit https://github.com/ErikGMatos/template-react-erik.git templatetemp &&  cp -r ./templatetemp/. ./$PROJECT  && rm -rf ./templatetemp && cd $PROJECT && yarn
 NOW=$(date +"%d-%m-%Y")
 USERNAME=$(whoami)
 sed -i -e 's/##USERNAME##/'$USERNAME'/g' src/pages/Dashboard/index.js package.json
 sed -i -e 's/##NOW##/'$NOW'/g' src/pages/Dashboard/index.js
 sed -i -e 's/nameproject/'$PROJECT'/g' package.json
-git add package.json src/pages/Dashboard/index.js && git commit -m 'feat(initial): Initial'
 echo -e "✔ Processo finalizado!"
 echo -e "✔ Projeto $PROJECT foi criado"
 echo -e "✔ Rode o comando abaixo"
